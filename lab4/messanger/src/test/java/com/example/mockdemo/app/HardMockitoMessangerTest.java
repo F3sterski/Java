3390c38d14ec46850d32924f2927dd578f619621
F3sterski <azielke@sigma.ug.edu.pl>
2015-03-25 16:58:01 +0100

    Mockito part 3

--- lab4/messanger/src/test/java/com/example/mockdemo/app/HardMockitoMessangerTest.java
+++ lab4/messanger/src/test/java/com/example/mockdemo/app/HardMockitoMessangerTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.invocation.InvocationOnMock;
@@ -16,13 +17,18 @@ import org.mockito.stubbing.Answer;
 
 @RunWith( MockitoJUnitRunner.class )
 public class HardMockitoMessangerTest {
+	
+	final String VALID_MESSAGE = "message";
+	final String VALID_ADRESS = "wp.pl";
+	final String INVALID_MESSAGE = "A";
+	final String INVALID_ADRESS = "wp.com";	
 
 	@Spy private MessageServiceSimpleImpl mockMS;
 	
 	@Test
 	public void ConnectionSatusFail(){
 		when(mockMS.checkConnection(anyString())).thenReturn(ConnectionStatus.FAILURE);
-		assertEquals(1,new Messenger(mockMS).testConnection("wp.pl"));
+		assertEquals(1,new Messenger(mockMS).testConnection(VALID_ADRESS));
 	}
 	@Test
 	public void ConnectionSatusSuccess(){
@@ -31,7 +37,7 @@ public class HardMockitoMessangerTest {
 		     public ConnectionStatus answer(InvocationOnMock invocation) {
 		         Object[] args = invocation.getArguments();
 		         Object mock = invocation.getMock();
-		         if(args[0].toString().endsWith(".pl")){
+		         if(args[0].toString().endsWith(VALID_ADRESS)){
 		        	 return ConnectionStatus.SUCCESS;
 		         }
 		         else{
@@ -41,7 +47,36 @@ public class HardMockitoMessangerTest {
 		     }
 		 });
 		
-		assertEquals(0,new Messenger(mockMS).testConnection("wp.pl"));
-	}	
+		assertEquals(0,new Messenger(mockMS).testConnection(VALID_ADRESS));
+	}
+	
+	@Test
+	public void sendTestSuccess() throws MalformedRecipientException{
+		ArgumentCaptor<String> server = ArgumentCaptor.forClass(String.class);
+		ArgumentCaptor<String> message = ArgumentCaptor.forClass(String.class);
+		
+		when(mockMS.send(VALID_ADRESS, VALID_MESSAGE)).thenReturn(SendingStatus.SENT);
+	    verify(mockMS).send(server.capture(), message.capture());	
+		   
+		   assertEquals(VALID_ADRESS, server.getValue());
+		   assertEquals(VALID_MESSAGE, message.getValue());
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
